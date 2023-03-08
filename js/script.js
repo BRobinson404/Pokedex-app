e2dd4d6ecb2914bb9dff8d64b73b9ccae6802c75
BRobinson404 <brobinso123@gmail.com>
2023-03-08 09:49:28 -0600

    created for() loop
    created a for() loop that writes the pokemonList table contents and attaches their height in feet

--- js/script.js
+++ js/script.js
@@ -19,4 +19,7 @@ let pokemonList=[
 
 //following code is a for() loop function that cycles through the
 // pokemmonList array and displays the name and the height in feet.
+let text='';
+for (let i=0; i< pokemonList.length; i++){
+    text = ' ' + pokemonList[i].name + ' (height:' + pokemonList[i].height + 'ft)';
 // adds a conditional to the loop if size greater than 5ft display special message
