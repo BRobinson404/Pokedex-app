03923dd200e548a1ad2fcc8a0556fe43926ce466
BRobinson404 <brobinso123@gmail.com>
2023-03-08 02:35:14 -0600

    created variable
    created variable call pokemonList and assigned it to an array of 3 objects and assigned 3 keys to the objects and their values.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 let pokemonList=[
+    {
+        name: 'bulbasaur', type: ['grass', 'poison'], height: 2
+    },
+    {
+        name: 'ivysaur', type: ['grass', 'poison'], height: 3
+    },
+    {
+        name: 'venusaur', type: ['grass', 'poison'], height: 6
+    }
+];
