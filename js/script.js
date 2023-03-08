b2900b05917d8bbc731b2ba08e6a6cced927e292
BRobinson404 <brobinso123@gmail.com>
2023-03-08 09:45:38 -0600

    indentation fix
    corrected indentation on PokemonList

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 let pokemonList=[
     {
-        name: 'bulbasaur', type: ['grass', 'poison'], height: 2
+        name: 'Bulbasaur',
+        type: ['grass', 'poison'],
+        height: 2
     },
     {
-        name: 'ivysaur', type: ['grass', 'poison'], height: 3
+        name: 'Ivysaur',
+        type: ['grass', 'poison'],
+        height: 3
     },
     {
-        name: 'venusaur', type: ['grass', 'poison'], height: 6
+        name: 'Venusaur',
+        type: ['grass', 'poison'],
+        height: 6
     }
 ];
