dae3b98b527f0d79d8cee90eb1ec8194ae0c07c5
BRobinson404 <brobinso123@gmail.com>
2023-03-16 03:10:01 -0500

    added spacing for readability

--- js/script.js
+++ js/script.js
@@ -16,15 +16,30 @@ let pokemonRepository = (function () {
             type: ['grass', 'poison'],
             height: 6
         }
-    ]
-    
+    ];
+
+//the getAll function returns the pokemonList from pokemonRepository
+
     function getAll () {
         return pokemonList
-    }
+    };
+
+//The add function pushes new pokemon to the pokemon to the pokemonList added "object" qualifier to prevent
+//incorrect data from being added. if data is not a correctly formatted object the console will log "pokemon is not correct"
+
     function add(pokemon) {
         pokemonList.push(pokemon);
     }
 
+//the showDetails function is used to log the input pokemon chosen from the addListItem button below vv
+
+
+//The addListItem function creates 3 variables:the <ul> pokemon-list, listItem, and button the latter two are 
+//created in the DOM. The inner text for the button is then set to pokemon.name and given the class "button-class"
+//and appended to listItem and then listItem is appended to pokemonList. Lastly an event listener was added to 
+//display the pokemons name in the console with a click using the showDetails functions
+
+//return block for the functions listed above in the IIFE
     return {
         getAll: getAll,
         add: add,
@@ -36,6 +51,7 @@ pokemonRepository.getAll().forEach(function(pokemon){
     document.write(pokemon.name + ' (' + pokemon.type + ') ' + pokemon.height + 'ft ');
 })
 // uncomment to add object to pokemonRepository
+// uncomment the code below to add object to pokemonRepository
 
 // pokemonRepository.add(
 // {
@@ -45,4 +61,4 @@ pokemonRepository.getAll().forEach(function(pokemon){
 // })
 
 // // uncomment to log the wrapped variable
-// console.log(pokemonRepository.getAll())
+// console.log(pokemonRepository.getAll())//     {
