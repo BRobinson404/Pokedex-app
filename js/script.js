7de795bbc9d4520294fed6993cc8d90c06341221
BRobinson404 <brobinso123@gmail.com>
2023-03-22 17:51:08 -0500

    changed fetch arguement
    corrected fetch argument to correct error

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ function loadList() {
 
 function loadDetails(item) {
   let url = item.detailsUrl;
-  return fetch(apiUrl).then(function (response) {
+  return fetch(url).then(function (response) {
     return response.json();
   }).then(function (details) {
     item.imageUrl = details.sprites.front_default;
