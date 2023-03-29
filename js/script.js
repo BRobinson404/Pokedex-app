8ca87f010a01982d75e489d07e918225e2789a3b
BRobinson404 <brobinso123@gmail.com>
2023-03-29 01:07:49 -0500

    deleting unused code
    removed unused model creation code because it is no implemented through bootstrap

--- js/script.js
+++ js/script.js
@@ -32,97 +32,11 @@ function loadList() {
       add(pokemon);
       
     });
-  }).catch(function (e) {
-    console.error(e);
-  })
-}
-
-function loadDetails(item) {
-  let url = item.detailsUrl;
-  return fetch(url).then(function (response) {
-    return response.json();
-  }).then(function (details) {
-    item.imageUrl = details.sprites.front_default;
-    item.height = details.height;
-    item.types = details.types.map((type) => type.type.name);
-  }).catch(function (e) {
-    console.error(e);
-  });
-}
-
-function showDetails(item) {
-  pokemonRepository.loadDetails(item).then(function () {
-   let modalContainer = document.querySelector ('#modal-container');
-
-   modalContainer.innerHTML = '';
-
-   let modal = document.createElement('div');
-   modal.classList.add ('modal');
-
-   let sprite = document.createElement('img');
-   sprite.classList.add('sprite');
-   sprite.src = item.imageUrl;
-
-   let closeButtonElement = document.createElement('button');
-   closeButtonElement.innerText = 'X';
-   closeButtonElement.addEventListener('click', hideModal)
-
-   let titleElement = document.createElement ('h1');
-   titleElement.innerText =  (item.name);
-
-   let contentElement = document.createElement ('p');
-
-   let pokemonTypes = "";
-
-     for (let i = 0; i < item.types.length; i++) {
       
-      pokemonTypes += item.types[i];
-      
-      if (i < item.types.length - 1) {
-          pokemonTypes += ", ";
       }
   }
 
-// add string to details (p)  
-  contentElement.innerText =('Height: ' + item.height + '\n' +  '\n' + 'Types: ' + pokemonTypes);
-
-// adding all content to modal and modal to container element
-  modal.appendChild (closeButtonElement);
-  modal.appendChild (titleElement);
-  modal.appendChild (contentElement);
-  modalContainer.appendChild (modal);
-  modal.appendChild (sprite);
-
-
-  modalContainer.classList.add('is-visible');
-
-
-function hideModal (){
-  modalContainer.classList.remove ('is-visible');
-}
-
-// hide modal if esc is pressed and is visible
-window.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')){
-    hideModal();
   }
-});
-
-// hide modal if container pressed
-modalContainer.addEventListener('click', (e) => {
-let target = e.target;
-if (target === modalContainer) {
-  hideModal();
-}
-});
-
-// show details if pokemon button is pressed
-document.querySelector ('button.button-class').addEventListener('click', () => {
-  showDetails ('Modal Title', 'Modal Content');
-});
-  });
-}
-
 
   return {
     add: add,
