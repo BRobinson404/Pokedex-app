d58c9de5edfa2d01c1c8092d9cf5ece2f2e959e0
BRobinson404 <brobinso123@gmail.com>
2023-03-29 01:11:51 -0500

    shifted to the bottom of the page from the top

--- js/script.js
+++ js/script.js
@@ -6,11 +6,60 @@ let pokemonRepository = (function () {
     pokemonList.push(pokemon);
   }
 
+  function getAll() {
+    return pokemonList;
+  }
+
+  function addListItem(pokemon) {
+    let pokemonListFolder = document.querySelector(".pokemon-list");
+    let createlistItem = document.createElement("li");
+    createlistItem.classList.add("list-group-item")
+    let button = document.createElement("button");
+    button.innerText= pokemon.name;
+    button.classList.add("btn-block");
+    button.classList.add("btn-primary");
+    button.classList.add("pokemon-button");
+    button.setAttribute('data-toggle', 'modal');
+    button.setAttribute('data-target', '#exampleModal');
+    createlistItem.classList.add('col-xl-3');
+    createlistItem.classList.add('col-lg-4');
+    createlistItem.classList.add('col-md-6');
+    createlistItem.appendChild(button);
+    pokemonListFolder.appendChild(createlistItem);
+    button.addEventListener('click', function () {
+      showDetails(pokemon);
     });
       
       }
   }
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
   }
 
   return {
